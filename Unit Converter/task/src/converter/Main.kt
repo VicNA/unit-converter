eb45a52baae8c7d942257c31024b5fda887e67b9
viktor-n <vnikolaeff989@gmail.com>
2022-11-02 12:12:53 +0300

    Project: Unit Converter. Stage 2/5. Complete

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,13 +1,12 @@
 package converter
 
 fun main() {
-    println(
-        """
-        145 centimeters is 1.45 meters
-        2 miles is 3.2187 kilometers
-        5.5 inches is 139.7 millimeters
-        12 degrees Celsius is 53.6 degrees Fahrenheit
-        3 pounds is 1.360776 kilograms
-        """.trimIndent()
-    )
+    print("Enter a number and a measure: ")
+    val (num, unit) = readln().split(' ')
+    val units = listOf("km", "kilometer", "kilometers")
+    if (unit.lowercase() in units) {
+        println("$num ${if (num == "1") units[1] else units[2]} is ${num.toInt().times(1000)} meters")
+    } else {
+        println("Wrong input")
+    }
 }
