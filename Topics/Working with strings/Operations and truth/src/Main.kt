388c6d37f6090691f1df2ff6f1c2eb341c9a12c4
viktor-n <vnikolaeff989@gmail.com>
2022-11-02 10:14:33 +0300

    Project: Unit Converter. Problem of the day. Complete

---
+++ Topics/Working with strings/Operations and truth/src/Main.kt
@@ -0,0 +1,1 @@
+fun main() = println(List(3) { readln() }.let { it[0].plus(it[1]) == it[2] })
---
+++ Topics/Working with strings/The information about a person/src/Main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val (firstName, lastName, age) = readln().split(" ")
+    println("${firstName.first()}. $lastName, $age years old")
+}
