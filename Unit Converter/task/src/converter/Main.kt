ae794e82652dc5ac0ecbd3ecf91f91d1aadfaf9a
viktor-n <vnikolaeff989@gmail.com>
2022-11-03 09:31:08 +0300

    Project: Unit Converter. Stage 3/6. Complete

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,12 +1,27 @@
 package converter
 
+enum class Unit(val supportUnits: List<String>, val rate: Double) {
+    Meters(listOf("m", "meter", "meters"), 1.0),
+    Kilometers(listOf("km", "kilometer", "kilometers"), 1000.0),
+    Centimeters(listOf("cm", "centimeter", "centimeters"), 0.01),
+    Millimeters(listOf("mm", "millimeter", "millimeters"), 0.001),
+    Miles(listOf("mi", "mile", "miles"), 1609.35),
+    Yards(listOf("yd", "yard", "yards"), 0.9144),
+    Feet(listOf("ft", "foot", "feet"), 0.3048),
+    Inches(listOf("in", "inch", "inches"), 0.0254);
+}
+
 fun main() {
-    print("Enter a number and a measure: ")
-    val (num, unit) = readln().split(' ')
-    val units = listOf("km", "kilometer", "kilometers")
-    if (unit.lowercase() in units) {
-        println("$num ${if (num == "1") units[1] else units[2]} is ${num.toInt().times(1000)} meters")
+    print("Enter a number and a measure of length: ")
+    val (inputNum, inputUnit) = readln().split(' ')
+    val unit = Unit.values().find { inputUnit.lowercase() in it.supportUnits }
+    if (unit != null) {
+        val units = unit.supportUnits
+        val num = inputNum.toDouble() * unit.rate
+        val s1 = if (inputNum.toDouble() == 1.0) units[1] else units[2]
+        val s2 = if (num == 1.0) "meter" else "meters"
+        println("${inputNum.toDouble()} $s1 is $num $s2")
     } else {
-        println("Wrong input")
+        println("Wrong input. Unknown unit $inputUnit")
     }
 }
