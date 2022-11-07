b987fd08c81b214ef571c5aa5971aecc74d884db
VicN <vnikolaeff989@gmail.com>
2022-11-07 15:32:59 +0300

    Project: Unit Converter. Stage 4/5. Complete

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,56 +1,46 @@
 package converter
 
-class Unit(val textUnit: String) {
+enum class Unit(val type: String, val supportUnits: List<String>, val rate: Double) {
+    METERS("length", listOf("m", "meter", "meters"), 1.0),
+    KILOMETERS("length", listOf("km", "kilometer", "kilometers"), 1000.0),
+    CENTIMETERS("length", listOf("cm", "centimeter", "centimeters"), 0.01),
+    MILLIMETERS("length", listOf("mm", "millimeter", "millimeters"), 0.001),
+    MILES("length", listOf("mi", "mile", "miles"), 1609.35),
+    YARDS("length", listOf("yd", "yard", "yards"), 0.9144),
+    FEET("length", listOf("ft", "foot", "feet"), 0.3048),
+    INCHES("length", listOf("in", "inch", "inches"), 0.0254),
 
-    enum class Length(val supportUnits: List<String>, val rate: Double) {
-        METERS(listOf("m", "meter", "meters"), 1.0),
-        KILOMETERS(listOf("km", "kilometer", "kilometers"), 1000.0),
-        CENTIMETERS(listOf("cm", "centimeter", "centimeters"), 0.01),
-        MILLIMETERS(listOf("mm", "millimeter", "millimeters"), 0.001),
-        MILES(listOf("mi", "mile", "miles"), 1609.35),
-        YARDS(listOf("yd", "yard", "yards"), 0.9144),
-        FEET(listOf("ft", "foot", "feet"), 0.3048),
-        INCHES(listOf("in", "inch", "inches"), 0.0254);
+    GRAMS("weight", listOf("g", "gram", "grams"), 1.0),
+    KILOGRAMS("weight", listOf("kg", "kilogram", "kilograms"), 1000.0),
+    MILLIGRAMS("weight", listOf("mg", "milligram", "milligrams"), 0.001),
+    POUNDS("weight", listOf("lb", "pound", "pounds"), 453.592),
+    OUNCES("weight", listOf("oz", "ounce", "ounces"), 28.3495);
+
+    companion object {
+        fun findUnit(string: String) = Unit.values().find { string in it.supportUnits }
+
+        fun equalsType(unitA: Unit, unitB: Unit) = unitA.type == unitB.type
     }
-
-    enum class Weight(val supportUnits: List<String>, val rate: Double) {
-        GRAMS(listOf("g", "gram", "grams"), 1.0),
-        KILOGRAMS(listOf("kg", "kilogram", "kilograms"), 1000.0),
-        MILLIGRAMS(listOf("mg", "milligram", "milligrams"), 0.001),
-        POUNDS(listOf("lb", "pound", "pounds"), 453.592),
-        OUNCES(listOf("oz", "ounce", "ounces"), 28.3495);
-    }
-
 }
 
-fun findUnit(string: String): Unit? {
-    val unit: Unit? = Length.values().find { string in it.supportUnits }
-    return unit ?: Weight.values().find { string in it.supportUnits }
-}
 
 fun main() {
     while (true) {
         print("Enter what you want to convert (or exit): ")
         val input = readln()
         if (input == "exit") break
-        val list = input.split(' ')
-        val srcUnit = findUnit(list[1])
-        val trgUnit = findUnit(list[3])
-        when {
-            srcUnit == null || trgUnit == null -> println("Conversion from ${list[1]} to feet is impossible")
-            trgUnit == null -> println("Conversion from ${list[3]} to feet is impossible")
-        }
-    }
-//    val (inputNum, inputUnit) = readln().split(' ')
-//    val unit = Unit.values().find { inputUnit.lowercase() in it.supportUnits }
-//    if (unit != null) {
-//        val units = unit.supportUnits
-//        val num = inputNum.toDouble() * unit.rate
-//        val s1 = if (inputNum.toDouble() == 1.0) units[1] else units[2]
-//        val s2 = if (num == 1.0) "meter" else "meters"
-//        println("${inputNum.toDouble()} $s1 is $num $s2")
-//    } else {
-//        println("Wrong input. Unknown unit $inputUnit")
-//    }
 
+        val list = input.split(' ')
+        val srcUnit = Unit.findUnit(list[1].lowercase())
+        val trgUnit = Unit.findUnit(list[3].lowercase())
+        if (srcUnit == null || trgUnit == null || !Unit.equalsType(srcUnit, trgUnit)) {
+            println("Conversion from ${srcUnit?.name?.lowercase() ?: "???"} to ${trgUnit?.name?.lowercase() ?: "???"} is impossible\n")
+            continue
+        }
+
+        val num = (list[0].toDouble() * srcUnit.rate) / trgUnit.rate
+        val s1 = if (list[0].toDouble() == 1.0) srcUnit.supportUnits[1] else srcUnit.supportUnits[2]
+        val s2 = if (num == 1.0) trgUnit.supportUnits[1] else trgUnit.supportUnits[2]
+        println("${list[0].toDouble()} $s1 is $num $s2\n")
+    }
 }
