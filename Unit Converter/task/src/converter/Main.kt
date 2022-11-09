474e559c3e44d177c9e412cdab302541504f2504
viktor-n <vnikolaeff989@gmail.com>
2022-11-09 17:31:17 +0300

    Project: Unit Converter. Stage 5/5. Progress v.3

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,41 +1,63 @@
 package converter
 
-enum class Length(val rate: (Double, Double) -> Double, vararg val names: String) {
-    METERS      ({ k, r -> (k * 1.0) / r },      "meter", "m", "meters"),
-    KILOMETERS  ({ k, r -> (k * 1000.0) / r },  "kilometer", "km", "kilometers"),
-    CENTIMETERS ({ k, r -> (k * 0.01) / r },    "centimeter", "cm", "centimeters"),
-    MILLIMETERS ({ k, r -> (k * 0.001) / r },   "millimeter", "mm", "millimeters"),
-    MILES       ({ k, r -> (k * 1609.35) / r }, "mile", "mi", "miles"),
-    YARDS       ({ k, r -> (k * 0.9144) / r },  "yard", "yd", "yards"),
-    FEET        ({ k, r -> (k * 0.3048) / r },  "foot", "ft", "feet"),
-    INCHES      ({ k, r -> (k * 0.0254) / r },  "inch", "in", "inches");
+enum class Length(val rate: Double, vararg val names: String) {
+    METERS      (1.0,     "meter", "m", "meters"),
+    KILOMETERS  (1000.0,  "kilometer", "km", "kilometers"),
+    CENTIMETERS (0.01,    "centimeter", "cm", "centimeters"),
+    MILLIMETERS (0.001,   "millimeter", "mm", "millimeters"),
+    MILES       (1609.35, "mile", "mi", "miles"),
+    YARDS       (0.9144,  "yard", "yd", "yards"),
+    FEET        (0.3048,  "foot", "ft", "feet"),
+    INCHES      (0.0254,  "inch", "in", "inches");
+
+    fun convertTo(value: Double, target: Length) = (value * rate) / target.rate
 
     val only = names.first()
     val plural = names.last()
+
+    companion object {
+        private val values = values()
+        fun find(string: String) = values.find { string in it.names }
+    }
 }
 
-enum class Weight(val rate: (Double, Double) -> Double, vararg val names: String) {
-    GRAMS       ({ k, r -> (k * 1.0) / r },      "gram", "g", "grams"),
-    KILOGRAMS   ({ k, r -> (k * 1000.0) / r },  "kilogram", "kg", "kilograms"),
-    MILLIGRAMS  ({ k, r -> (k * 0.001) / r },   "milligram", "mg", "milligrams"),
-    POUNDS      ({ k, r -> (k * 453.592) / r }, "pound", "lb", "pounds"),
-    OUNCES      ({ k, r -> (k * 28.3495) / r }, "ounce", "oz", "ounces");
+enum class Weight(val rate: Double, vararg val names: String) {
+    GRAMS       (1.0,     "gram", "g", "grams"),
+    KILOGRAMS   (1000.0,  "kilogram", "kg", "kilograms"),
+    MILLIGRAMS  (0.001,   "milligram", "mg", "milligrams"),
+    POUNDS      (453.592, "pound", "lb", "pounds"),
+    OUNCES      (28.3495, "ounce", "oz", "ounces");
+
+    fun convertTo(value: Double, target: Weight) = (value * rate) / target.rate
 
     val only = names.first()
     val plural = names.last()
+
+    companion object {
+        private val values = values()
+        fun find(string: String) = values.find { string in it.names }
+    }
 }
 
-enum class Temperature(val rate: Double, vararg val names: String) {
-    CELSIUS     (1.0,    "degree Celsius", "c", "dc", "celsius", "degrees Celsius"),
-    FAHRENHEIT  (1000.0, "degree Fahrenheit", "r", "df", "fahrenheit", "degrees Fahrenheit"),
-    KELVINS     (0.001,  "kelvin", "k", "kelvins");
+enum class Temperature(vararg val names: String) {
+    CELSIUS     ("degree Celsius", "c", "dc", "celsius", "degrees Celsius"),
+    FAHRENHEIT  ("degree Fahrenheit", "r", "df", "fahrenheit", "degrees Fahrenheit"),
+    KELVINS     ("kelvin", "k", "kelvins");
+
+
 
     val only = names.first()
     val plural = names.last()
+
+    companion object {
+        private val values = values()
+        fun find(string: String) = values.find { string in it.names }
+    }
 }
 
 class Converter(from: String, to: String) {
-    val fromUnit = find(from)
+//    val fromUnit = Length.find(from) ?: Weight.find(from) ?: Temperature.find(from)
+    val fromUnit = from.let { Length.find(it) ?: Weight.find(it) ?: Temperature.find(it) }
     val toUnit = find(to)
 
     fun isPossible(): Boolean {
@@ -48,16 +70,16 @@ class Converter(from: String, to: String) {
     }
 
     fun convertTo() {
-        return when ((fromUnit, toUnit)) {
-            Temperature.CELSIUS
+        return when (toUnit) {
+            fromUnit == Temperature.CELSIUS && toUnit == Temperature.KELVINS ->
         }
     }
 
-    companion object {
-        fun find(string: String) = Length.values().find { string in it.names } ?:
-            Weight.values().find { string in it.names } ?:
-            Temperature.values().find { string in it.names }
-    }
+//    companion object {
+//        fun find(string: String) = Length.values().find { string in it.names } ?:
+//            Weight.values().find { string in it.names } ?:
+//            Temperature.values().find { string in it.names }
+//    }
 }
 
 
@@ -70,11 +92,8 @@ fun main() {
         if (input == "exit") break
 
         val list = input.split(' ')
-        println(Temperature.values().asSequence()
-            .flatMap { it.names.asSequence().map { name -> name to it } }
-            .toMap()["miles"].)
-//        s1 = if (list.size > 4) list[2].lowercase() else list[1].lowercase()
-//        s2 = list.last()
+        s1 = if (list.size > 4) list[2].lowercase() else list[1].lowercase()
+        s2 = list.last()
 //        val converter = Converter(s1, s2)
 //        if (converter.isPossible()) {
 //            println("Conversion from ${srcUnit?.name?.lowercase() ?: "???"} to ${trgUnit?.name?.lowercase() ?: "???"} is impossible\n")
