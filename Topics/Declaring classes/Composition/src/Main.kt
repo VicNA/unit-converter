e21a48104f74c7bcec35db9db6ee0146c010c9f2
viktor-n <vnikolaeff989@gmail.com>
2022-11-11 15:56:47 +0300

    Project: Unit Converter. Repeat topics. Complete

---
+++ Topics/Declaring classes/Composition/src/Main.kt
@@ -0,0 +1,8 @@
+class OperatingSystem {
+    var name: String = "Windows"
+}
+
+class DualBoot {
+    var primaryOs: OperatingSystem = OperatingSystem()
+    var secondaryOs: OperatingSystem = OperatingSystem()
+}
